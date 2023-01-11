55523136ac890c3bd42dd478c3ff85e154a07ba2
onisim_albarov <onisim93@gmail.com>
2023-01-11 12:30:40 +0400

    fixed H2ConnectionFactory.java and ThreadSafeSingleton.java

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -2,24 +2,18 @@ package com.mjc.stage2.impl;
 
 import com.mjc.stage2.ConnectionFactory;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Objects;
 import java.util.Properties;
 
 public class H2ConnectionFactory implements ConnectionFactory {
     @Override
     public Connection createConnection() {
         Properties properties = new Properties();
-        String rootPath = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("")).getPath();
-        try (FileInputStream is = new FileInputStream(rootPath + "h2database.properties")) {
+        try (InputStream is = H2ConnectionFactory.class.getClassLoader().getResourceAsStream("h2database.properties")) {
             properties.load(is);
             return DriverManager.getConnection(properties.getProperty("db_url"), properties.getProperty("user"), properties.getProperty("password"));
         } catch (IOException | SQLException e) {
